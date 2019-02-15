b09bba4806806023f0fbf08b0154c108ddeb20d7
Marcel-Velez <marcel.velez1997@hotmail.com>
2019-02-15 11:06:43 +0100

    Working on displaying players turn

--- app/src/main/java/com/example/mytictactoe/Game.java
+++ app/src/main/java/com/example/mytictactoe/Game.java
@@ -22,7 +22,7 @@ public class Game {
     }
 
     public Tile choose(int row, int column) {
-        state = board[row][column];
+        Tile state = board[row][column];
 
         if(state == Tile.BLANK) {
             if (playerOneTurn) {
@@ -36,5 +36,68 @@ public class Game {
             }
         }
         return Tile.INVALID;
+    }
+
+    public GameState won() {
+        if (rowWin() == GameState.PLAYER_ONE || rowWin() == GameState.PLAYER_TWO ) {
+            return rowWin();
+        } else if (colWin() == GameState.PLAYER_ONE || colWin() == GameState.PLAYER_TWO ) {
+            return colWin();
+        } else if (diaWin() == GameState.PLAYER_ONE || diaWin() == GameState.PLAYER_TWO ) {
+            return diaWin();
+        } else if (drawCheck() == GameState.IN_PROGRESS || drawCheck() == GameState.DRAW) {
+            return drawCheck();
+        } else {
+            return GameState.IN_PROGRESS;
+        }
+    }
+
+    private GameState rowWin() {
+        if ((board[0][0]== Tile.CROSS && board[1][0]== Tile.CROSS && board[2][0]== Tile.CROSS) ||
+                (board[0][1] == Tile.CROSS && board[1][1] == Tile.CROSS && board[2][1] == Tile.CROSS) ||
+                (board[0][2] == Tile.CROSS && board[1][2] == Tile.CROSS && board[2][2] == Tile.CROSS)) {
+            return GameState.PLAYER_ONE;
+        } else if ((board[0][0]== Tile.CIRCLE && board[1][0]== Tile.CIRCLE && board[2][0]== Tile.CIRCLE) ||
+                (board[0][1] == Tile.CIRCLE && board[1][1] == Tile.CIRCLE && board[2][1] == Tile.CIRCLE) ||
+                (board[0][2] == Tile.CIRCLE && board[1][2] == Tile.CIRCLE && board[2][2] == Tile.CIRCLE)) {
+            return GameState.PLAYER_TWO;
+        }
+        return GameState.IN_PROGRESS;
+    }
+
+
+    private GameState colWin() {
+        if ((board[0][0]== Tile.CROSS && board[0][1]== Tile.CROSS && board[0][2]== Tile.CROSS) ||
+                (board[1][0] == Tile.CROSS && board[1][1] == Tile.CROSS && board[1][2] == Tile.CROSS) ||
+                (board[2][0] == Tile.CROSS && board[2][1] == Tile.CROSS && board[2][2] == Tile.CROSS)) {
+            return GameState.PLAYER_ONE;
+        } else if ((board[0][0]== Tile.CIRCLE && board[0][1]== Tile.CIRCLE && board[0][2]== Tile.CIRCLE) ||
+                (board[1][0] == Tile.CIRCLE && board[1][1] == Tile.CIRCLE && board[1][2] == Tile.CIRCLE) ||
+                (board[2][0] == Tile.CIRCLE && board[2][1] == Tile.CIRCLE && board[2][2] == Tile.CIRCLE)) {
+            return GameState.PLAYER_TWO;
+        }
+        return GameState.IN_PROGRESS;
+    }
+
+    private GameState diaWin() {
+        if ((board[0][0] == Tile.CROSS && board[1][1] == Tile.CROSS && board[2][2] == Tile.CROSS) ||
+                (board[2][0] == Tile.CROSS && board[1][1] == Tile.CROSS && board[0][2] == Tile.CROSS)) {
+            return GameState.PLAYER_ONE;
+        } else if ((board[0][0] == Tile.CIRCLE && board[1][1] == Tile.CIRCLE && board[2][2] == Tile.CIRCLE) ||
+                (board[2][0] == Tile.CIRCLE && board[1][1] == Tile.CIRCLE && board[0][2] == Tile.CIRCLE)) {
+            return GameState.PLAYER_TWO;
+        }
+        return GameState.IN_PROGRESS;
+    }
+
+    private GameState drawCheck() {
+        for (int i=0; i<BOARD_SIZE; i++) {
+            for (int j=0; j<BOARD_SIZE; j++) {
+                if (board[i][j] == Tile.BLANK) {
+                    return GameState.IN_PROGRESS;
+                }
+            }
+        }
+        return GameState.DRAW;
     }
 }
--- app/src/main/java/com/example/mytictactoe/MainActivity.java
+++ app/src/main/java/com/example/mytictactoe/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.mytictactoe;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Game game;
@@ -14,29 +17,106 @@ public class MainActivity extends AppCompatActivity {
         game = new Game();
     }
 
+    private int[] getCoord(View view) {
+        String id = (String) view.getTag();
+
+        switch (id) {
+            case "button0": int[] a = {0,0};
+                            return a;
+
+            case "button1": int[] b = {1,0};
+                            return b;
+
+            case "button2": int[] c = {2,0};
+                            return c;
+
+            case "button3": int[] d = {0,1};
+                            return d;
+
+            case "button4": int[] e = {1,1};
+                            return e;
+
+            case "button5": int[] f = {2,1};
+                            return f;
+
+            case "button6": int[] g = {0,2};
+                            return g;
+
+            case "button7": int[] h = {1,2};
+                            return h;
+
+            case "button8": int[] i = {2,2};
+                            return i;
+
+        }
+        int[] exception = {0,0};
+        return exception;
+    }
+
     public void tileClicked(View view) {
-        int id, row, column;
-        Tile state;
+        if (game.won() == GameState.IN_PROGRESS) {
+            Tile state;
 
+            int resID = getResources().getIdentifier("mainText", "id", getPackageName());
+            TextView maintText = (TextView) findViewById(resID);
 
-        id = view.getId();
-        row = (int) (id / 3);
-        column = id % 3;
+            int[] coord = getCoord(view);
+            state = game.choose(coord[0], coord[1]);
 
-        state = game.choose(row, column);
-
-        switch(state) {
-            case CROSS:     //blabla;
-                            break;
-            case CIRCLE     //something;
-                            break;
-            case INVALID    //something;
-                            break;
+            Button clicked = (Button) view;
+            switch (state) {
+                case CROSS:
+                    clicked.setText("X");
+                    maintText.setText("Player 2's Turn");
+                    //clicked.setTextColor(Color.holo_purple);
+                    break;
+                case CIRCLE:
+                    clicked.setText("O");
+                    maintText.setText("Player 1's Turn");
+                    //clicked.setTextColor(getApplication().getResources().getColor(R.color.red));
+                    break;
+                case INVALID:   //something;
+                    break;
+            }
+        }
+        if (game.won() != GameState.IN_PROGRESS) {
+            unableButtons();
+            int resID = getResources().getIdentifier("button4", "id", getPackageName());
+            Button leButton = (Button) findViewById(resID);
+            GameState state = game.won();
+            if  (state == GameState.DRAW) {
+                leButton.setText("DRAW");
+            } else if (state == GameState.PLAYER_ONE) {
+                leButton.setText("PLAYER ONE WON!");
+            } else if (state == GameState.PLAYER_TWO) {
+                leButton.setText("PLAYER TWO WON!");
+            }
         }
     }
 
-    public void resetClicked() {
+    private void unableButtons() {
+        int resID = getResources().getIdentifier("GridLayout", "id", getPackageName());
+        android.support.v7.widget.GridLayout mlayout = (android.support.v7.widget.GridLayout) findViewById(resID);
+
+        int count = mlayout.getChildCount();
+        for(int i = 0 ; i < count ; i++) {
+            Button button = (Button) mlayout.getChildAt(i);
+            button.setEnabled(false);
+        }
+    }
+
+    public void resetClicked(View view) {
         game = new Game();
+
+        int resID = getResources().getIdentifier("GridLayout", "id", getPackageName());
+        android.support.v7.widget.GridLayout mlayout = (android.support.v7.widget.GridLayout) findViewById(resID);
+
+        int count = mlayout.getChildCount();
+        for(int i = 0 ; i < count ; i++){
+            Button button = (Button) mlayout.getChildAt(i);
+            button.setText("");
+            button.setEnabled(true);
+        }
 
     }
 
